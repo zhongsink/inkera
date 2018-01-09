d71c284573e774d2651ccab3c440c44296188700
zhongsink <zhongink@gmail.com>
2018-01-09 23:18:59 +0800

    add logger

--- app/server/middlewares/logger.js
+++ app/server/middlewares/logger.js
@@ -1,9 +1,9 @@
-
+let utilLogger = require('../utils/Logger');
 const logger = async (ctx, next) => {
   const now = new Date();
   await next();
   const ms = new Date() - now;
-  console.log(`[${ctx.method}][${ms}ms] ${ctx.path}`);
+  utilLogger.info(`[${ctx.method}][${ms}ms] ${ctx.path}`);
 };
 
 export default logger;
